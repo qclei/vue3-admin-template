0dfc06b65bd8df5e324cedee10090ff46139354d
qclei <1486717078@qq.com>
2023-07-26 16:11:42 +0800

    提交没有格式化的代码

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="box">
+    <h1>Hello work</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const str = '我爱你祖国'
+</script>
+
+<style lang="scss" scoped>
+.box {
+  width: 600px;
+  height: 400px;
+  background: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
