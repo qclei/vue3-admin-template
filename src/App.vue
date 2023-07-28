77c3879816c4d7b25ade942baae0f6cc7c33e713
qclei <1486717078@qq.com>
2023-07-28 17:44:52 +0800

    build: svg组件全局配置

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,11 @@
 <template>
   <div>
-    <el-button type="primary" size="default" :icon="Plus">主要按钮</el-button>
-    <el-button type="success" size="small" :icon="Edit">编辑按钮</el-button>
-    <el-button type="danger" size="small" :icon="Delete">删除按钮</el-button>
-    <el-pagination
-      :page-sizes="[100, 200, 300, 400]"
-      small="small"
-      disabled="disabled"
-      background="background"
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="400"
-    />
+    <svg-icon name="phone" color="red"></svg-icon>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Plus, Edit, Delete } from '@element-plus/icons-vue'
+
 </script>
+
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import 'element-plus/dist/index.css'
 //@ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
+import 'virtual:svg-icons-register'
+// 引入之定义插件对象：注册整个项目的全局组件
+import gloablComponent from '@/components/index'
+
 import App from '@/App.vue'
 
 // 获取应用实例对象
@@ -15,5 +19,7 @@ app.use(ElementPlus, {
 })
 // 安装element-plus插件
 app.use(ElementPlus)
+//安装自定义插件对象
+app.use(gloablComponent)
 // 将应用挂载到挂载点上
 app.mount('#app')
