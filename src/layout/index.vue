01ced538d135d2977e2acf96bc1c1ccb9dcf7061
qclei <1486717078@qq.com>
2023-08-02 12:07:14 +0800

    feat: 静态菜单搭建

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -3,6 +3,26 @@
     <!-- 左侧菜单-->
     <div class="layout_slider">
       <Logo></Logo>
+      <!-- 展示菜单-->
+      <el-scrollbar class="scrollbar">
+        <!-- 菜单组件-->
+        <el-menu
+          background-color="{{ $base-menu-background }}"
+          text-color="{{ $base-menu-text-color }}"
+        >
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-menu-item index="2">数据大屏</el-menu-item>
+          <!-- 折叠菜单 -->
+          <el-sub-menu index="3">
+            <template #title>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="3-1">用户管理</el-menu-item>
+            <el-menu-item index="3-2">角色管理</el-menu-item>
+            <el-menu-item index="3-3">菜单管理</el-menu-item>
+          </el-sub-menu>
+        </el-menu>
+      </el-scrollbar>
     </div>
     <!-- 顶部导航-->
     <div class="layout_tabbar">456</div>
@@ -22,10 +42,15 @@ import Logo from './logo/index.vue'
 .layout_container {
   width: 100%;
   height: 100vh;
+  color: white;
   .layout_slider {
     width: $base-menu-width;
     height: 100vh;
     background: $base-menu-background;
+    .scrollbar {
+      width: 100%;
+      height: calc(100vh - $base-menu-logo-height);
+    }
   }
   .layout_tabbar {
     position: fixed;
